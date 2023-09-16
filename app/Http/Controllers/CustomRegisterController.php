bdf24d71200f5ec114faf378030fc19405eb07f3
Princia Benagon <111951056+princiabenegon@users.noreply.github.com>
2023-09-17 02:22:04 +0530

    first commit

---
+++ app/Http/Controllers/CustomRegisterController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\SendRegistrationToCodeIgniter;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Listeners\SendRegistrationDataToCodeIgniter;
+
+class CustomRegisterController extends Controller
+{
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        $user = $this->create($request->all());
+        dd($user->name);
+        event(new SendRegistrationToCodeIgniter($user));
+
+        // You can customize the response or redirection here
+    }
+
+    protected function create(array $data)
+    {
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'passwordview' => $data['password'],
+        ]);
+
+        return $user;
+    }
+
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+}
---
+++ app/Listeners/SendRegistrationDataToCodeIgniter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Events\SendRegistrationToCodeIgniter;
+use Illuminate\Support\Facades\Http;
+
+class SendRegistrationDataToCodeIgniter
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(SendRegistrationToCodeIgniter  $event)
+    {
+        $user = $event->user;
+        // Prepare registration data from the $user object
+        $registrationData = [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => $user->passwordview
+        ];
+        $response = Http::post('http://codelara/register', $registrationData);
+        if ($response->successful()) {
+            return "success";
+        } else {
+           return "failed";
+        }
+    }
+}
